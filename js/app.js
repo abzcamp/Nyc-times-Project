185e25780d18507596ec0be5f6ec434b2be378ec
Abzcamp <abzcamp@gmail.com>
2019-08-08 17:55:48 -0400

    feat: added styles mediaquery and layout

--- js/app.js
+++ js/app.js
@@ -10,14 +10,35 @@
             $.getJSON(`https://api.nytimes.com/svc/topstories/v2/${newsInput}.json?api-key=8jjm1KEG23fQSFGvGbIWBOAyt2Yd6jqW`)
             .done(function(data) {
             console.log(data)
-                $.each(data.results, function () {
-                    let newsTitle = this.abstract
-                    let picURL = this.multimedia[4].url
-                    // create div  to hold album cover and name
-                    $('.news').append(`<img src="${picURL}"><p>${newsTitle}</p>`).children().addClass('remove')
-                    
-                    
-                    })
+           let array = []
+           for (let i=0; i < 12; i++){
+               array.push(data.results[i])
+               console.log(array)
+           }
+           
+
+                $.each(array, function(index, value) {
+                    console.log("index",index)
+                    console.log("value",value)
+                    console.log(value.title)
+
+                     
+                    console.log(data.results)
+                    let newsTitle = value.abstract
+                console.log(newsTitle)
+                let picURL = this.multimedia[4].url
+                console.log(this)
+                $('.news').append(`<div class="attach"><img src="${picURL}"><p class= target>${newsTitle}</p></div>`)
+
+
+                
+                
+                // create div  to hold album cover and name
+                
+                
+                })
+
+           
                 })
                 .fail(function() {
                 alert("Page not Found") 
